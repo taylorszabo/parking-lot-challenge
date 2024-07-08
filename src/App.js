9544986e65972c458eebe3abb9028208789bc852
easyseven-vehikl <132284601+easyseven-vehikl@users.noreply.github.com>
2024-07-08 16:20:02 -0400

    create basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Parking Garage Management</h1>
+            <div>
+                <h2>Arrival</h2>
+                <input
+                    type="text"
+                    value={licensePlate}
+                    placeholder="Enter license plate"
+                />
+                <button>Enter Garage</button>
+            </div>
+            <div>
+                <h2>Current Cars</h2>
+            </div>
+        </div>
+    );
+};
 
 export default App;
